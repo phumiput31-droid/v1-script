f1e19cde28652845deae08e71e0104f9467bc7c5
phumiput31-droid <phumiput31@gmail.com>
2025-10-09 17:15:08 +0700

    Create script V2.lua

---
+++ script V2.lua
@@ -0,0 +1,15 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local Frame = Instance.new("Frame")
+
+--Properties:
+
+Frame.Parent = game.StarterGui.ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(168, 168, 168)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.18733944, 0, 0.307534933, 0)
+Frame.Size = UDim2.new(0, 416, 0, 214)
