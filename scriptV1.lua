c12533d0e9d048e61821b2efe697f379b2f82afa
phumiput31-droid <phumiput31@gmail.com>
2026-02-09 01:39:44 +0700

    Update scriptV1.lua

--- scriptV1.lua
+++ scriptV1.lua
@@ -1,11 +1,12 @@
 return {
-    {Id = 16518256559, Script = "zxYEzXue", Tag = "Mega Obby Fun"},
-    {Id = 5972698540,  Script = "qLHgiFzR", Tag = "Easy Obby"},
-    {Id = 128646334704030, Script = "WjL2zGZB", Tag = "Fruit Stage Tower"},
+    {Id = 16518256559, Script = "zxYEzXue", Tag = "Fun for Kid"},
+    {Id = 94249547303335, Script = "ye6X0VeA", Tag = "GG map Brooo"},
+    {Id = 5972698540,  Script = "qLHgiFzR", Tag = "Easy l"},
+    {Id = 128646334704030, Script = "WjL2zGZB", Tag = "Fruit lol"},
     {Id = 8003084678,  Script = "xTNKWT8X", Tag = "Obby Map A"},
     {Id = 5946446414,  Script = "xTNKWT8X", Tag = "Obby Map B"},
     {Id = 17188200379, Script = "MhDSQf6i", Tag = "Combat Map 1"},
-    {Id = 10449761463, Script = "FcJp64bg", Tag = "Combat Map 2"},
-    {Id = 85574678542712, Script = "uV8rDx9t", Tag = "Flash Speedster Pro"},
-     {Id = 135619589726492, Script = "thJFJBFL", Tag = "Auto Win (BETA)"},
+    {Id = 10449761463, Script = "FcJp64bg", Tag = "Combat Map GG2"},
+    {Id = 85574678542712, Script = "uV8rDx9t", Tag = "Speedster Pro"},
+    {Id = 135619589726492, Script = "thJFJBFL", Tag = "Auto Win (BETA)"},
 }
