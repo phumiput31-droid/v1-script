5f4dfa3c72c614d826019ba998a00162854bf77b
phumiput31-droid <phumiput31@gmail.com>
2026-02-09 18:18:21 +0700

    Update scriptV1.lua

--- scriptV1.lua
+++ scriptV1.lua
@@ -1,12 +1,21 @@
 return {
-    {Id = 16518256559, Script = "zxYEzXue", Tag = "Fun for Kid"},
-    {Id = 94249547303335, Script = "BH7Lgjk2", Tag = "GG map Brooo"},
-    {Id = 5972698540,  Script = "qLHgiFzR", Tag = "Easy l"},
-    {Id = 128646334704030, Script = "WjL2zGZB", Tag = "Fruit lol"},
-    {Id = 8003084678,  Script = "xTNKWT8X", Tag = "Obby Map A"},
-    {Id = 5946446414,  Script = "xTNKWT8X", Tag = "Obby Map B"},
-    {Id = 17188200379, Script = "MhDSQf6i", Tag = "Combat Map 1"},
-    {Id = 10449761463, Script = "FcJp64bg", Tag = "Combat Map GG2"},
-    {Id = 85574678542712, Script = "uV8rDx9t", Tag = "Speedster Pro"},
-    {Id = 135619589726492, Script = "thJFJBFL", Tag = "Auto Win (BETA)"},
+    -- ส่วนตั้งค่า Key System
+    Config = {
+        Key = "GoodBoy1234567890987654321qwer10101", -- เปลี่ยน Key ตรงนี้
+        Url = "https://direct-link.net/3377873/oTUKqgDxRf6S" -- เปลี่ยน Link ตรงนี้
+    },
+
+    -- ส่วนรายชื่อเกม (Script Hub) ที่คุณส่งมา
+    Scripts = {
+        {Id = 16518256559, Script = "zxYEzXue", Tag = "Fun for Kid"},
+        {Id = 94249547303335, Script = "BH7Lgjk2", Tag = "GG map Brooo"},
+        {Id = 5972698540,  Script = "qLHgiFzR", Tag = "Easy l"},
+        {Id = 128646334704030, Script = "WjL2zGZB", Tag = "Fruit lol"},
+        {Id = 8003084678,  Script = "xTNKWT8X", Tag = "Obby Map A"},
+        {Id = 5946446414,  Script = "xTNKWT8X", Tag = "Obby Map B"},
+        {Id = 17188200379, Script = "MhDSQf6i", Tag = "Combat Map 1"},
+        {Id = 10449761463, Script = "FcJp64bg", Tag = "Combat Map GG2"},
+        {Id = 85574678542712, Script = "uV8rDx9t", Tag = "Speedster Pro"},
+        {Id = 135619589726492, Script = "thJFJBFL", Tag = "Auto Win (BETA)"}
+    }
 }
