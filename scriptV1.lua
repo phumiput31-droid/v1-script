c6f0a381fd6dc7d062ca2b78656952357357d3d7
phumiput31-droid <phumiput31@gmail.com>
2026-02-28 00:22:41 +0700

    Update scriptV1.lua

--- scriptV1.lua
+++ scriptV1.lua
@@ -15,7 +15,7 @@ return {
         {Id = 8003084678,  Script = "xTNKWT8X", Tag = "Obby Map A"},
         {Id = 5946446414,  Script = "xTNKWT8X", Tag = "Obby Map B"},
         {Id = 17188200379, Script = "MhDSQf6i", Tag = "Combat Map 1"},
-        {Id = 10449761463, Script = "FcJp64bg", Tag = "Combat Map GG2"},
+        {Id = 10449761463, Script = "FcJp64bg", Tag = "CoMo Map GG2"},
         {Id = 135125539939312, Script = "RDqv0xCd", Tag = "Easy bro nope"},
         {Id = 119918777507638, Script = "ZpDLTbKD", Tag = "it Beta but Good?"},
         {Id = 85574678542712, Script = "uV8rDx9t", Tag = "Speedster Pro"},
